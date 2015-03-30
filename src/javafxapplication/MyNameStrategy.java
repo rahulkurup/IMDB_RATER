d92d86d380b6f9e14918ae1d38ac58d622b16a42
Rahul Kurup <formyrahul@gmail.com>
2015-03-30 22:20:12 +0530

    Adding All the source Files
    Source Files Added

---
+++ src/javafxapplication/MyNameStrategy.java
@@ -0,0 +1,46 @@
+package javafxapplication;
+
+import org.codehaus.jackson.map.MapperConfig;
+import org.codehaus.jackson.map.PropertyNamingStrategy;
+import org.codehaus.jackson.map.introspect.AnnotatedField;
+import org.codehaus.jackson.map.introspect.AnnotatedMethod;
+
+public class MyNameStrategy extends PropertyNamingStrategy
+ {
+  @Override
+  public String nameForField(MapperConfig
+                       config,
+   AnnotatedField field, String defaultName) {
+     return convert(defaultName);
+
+  }
+  @Override
+  public String nameForGetterMethod(MapperConfig
+                       config,
+   AnnotatedMethod method, String defaultName) {
+     return convert(defaultName);
+  }
+
+  @Override
+  public String nameForSetterMethod(MapperConfig
+                       config,
+    AnnotatedMethod method, String defaultName) {
+   String a = convert(defaultName); 
+   return a;
+  }
+
+  public String convert(String defaultName )
+  {
+   char[] arr = defaultName.toCharArray();
+   if(arr.length !=0)
+   {
+    if ( Character.isLowerCase(arr[0])){
+     char upper = Character.toUpperCase(arr[0]);
+     arr[0] = upper;
+    }
+   }
+   return new StringBuilder().append(arr).toString();
+  }
+
+ }
+
